3f32073a5aeb20cfd3134d5624f020a3d78d4a11
Mortadha Naser <mortadha114@gmail.com>
2023-07-07 18:03:23 +0300

    just formated code

--- lib/Features/auth/presentation/cubits/auth_cubit/auth_state.dart
+++ lib/Features/auth/presentation/cubits/auth_cubit/auth_state.dart
@@ -3,27 +3,39 @@ part of 'auth_cubit.dart';
 abstract class AuthState {}
 
 class AuthInitial extends AuthState {}
+
 class RegisterLoading extends AuthState {}
+
 class RegisterSuccess extends AuthState {}
+
 class RegisterFailure extends AuthState {
   final String message;
   RegisterFailure({required this.message});
 }
+
 class CompleteProfileLoading extends AuthState {}
+
 class CompleteProfileSuccess extends AuthState {}
+
 class CompleteProfileFailure extends AuthState {
   final String message;
   CompleteProfileFailure({required this.message});
 }
+
 class SignInLoading extends AuthState {}
+
 class SignInSuccess extends AuthState {}
+
 class SignInFailure extends AuthState {
   final String message;
   SignInFailure({required this.message});
-  }
+}
+
 class ResetPasswordLoading extends AuthState {}
+
 class ResetPasswordSuccess extends AuthState {}
+
 class ResetPasswordFailure extends AuthState {
   final String message;
   ResetPasswordFailure({required this.message});
-  }
+}
