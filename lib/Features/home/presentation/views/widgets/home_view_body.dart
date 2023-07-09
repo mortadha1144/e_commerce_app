05f7e4ff6e0eb2ce0779df28b35d31b6a58116b2
Mortadha Naser <mortadha114@gmail.com>
2023-07-09 20:50:53 +0300

    special offers ui section done

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,11 @@
-import 'package:e_commerce_app/size_config.dart';
+import 'package:e_commerce_app/Features/home/presentation/views/widgets/section_title.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../size_config.dart';
 import 'categories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
-import 'section_title.dart';
+import 'special_offers.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -22,35 +23,11 @@ class HomeViewBody extends StatelessWidget {
             SizedBox(height: getProportionateScreenWidth(30)),
             const Categories(),
             SizedBox(height: getProportionateScreenWidth(30)),
+            const SpecialOffers(),
+            SizedBox(height: getProportionateScreenWidth(30)),
             SectionTitle(
-              text: 'Special for you',
+              text: 'Popular Product',
               onPress: () {},
-            ),
-            SizedBox(
-              width: getProportionateScreenWidth(242),
-              height: getProportionateScreenWidth(100),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Stack(
-                  children: [
-                    Image.asset(
-                      'assets/images/Image Banner 2.png',
-                      fit: BoxFit.cover,
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          const Color(0xFF343434).withOpacity(0.4),
-                          const Color(0xFF343434).withOpacity(0.15),
-                        ],
-                      )),
-                    )
-                  ],
-                ),
-              ),
             )
           ],
         ),
---
+++ lib/Features/home/presentation/views/widgets/special_offers.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../size_config.dart';
+import 'section_title.dart';
+
+class SpecialOffers extends StatelessWidget {
+  const SpecialOffers({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SectionTitle(
+          text: 'Special for you',
+          onPress: () {},
+        ),
+        SizedBox(height: getProportionateScreenWidth(20)),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SpecialOfferCard(
+                category: 'Smartphones',
+                numOfBrands: 18,
+                image: 'assets/images/Image Banner 2.png',
+                onPress: () {},
+              ),
+              SpecialOfferCard(
+                category: 'Fashions',
+                numOfBrands: 24,
+                image: 'assets/images/Image Banner 3.png',
+                onPress: () {},
+              ),
+              SizedBox(width: getProportionateScreenWidth(20))
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SpecialOfferCard extends StatelessWidget {
+  const SpecialOfferCard({
+    super.key,
+    required this.category,
+    required this.image,
+    required this.numOfBrands,
+    required this.onPress,
+  });
+
+  final String category, image;
+  final int numOfBrands;
+  final VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: getProportionateScreenWidth(20)),
+      child: SizedBox(
+        width: getProportionateScreenWidth(242),
+        height: getProportionateScreenWidth(100),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Stack(
+            children: [
+              Image.asset(
+                image,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    const Color(0xFF343434).withOpacity(0.4),
+                    const Color(0xFF343434).withOpacity(0.15),
+                  ],
+                )),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(15),
+                  vertical: getProportionateScreenWidth(10),
+                ),
+                child: Text.rich(
+                  TextSpan(
+                    style: const TextStyle(color: Colors.white),
+                    children: [
+                      TextSpan(
+                        text: '$category\n',
+                        style: TextStyle(
+                          fontSize: getProportionateScreenWidth(18),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(text: '$numOfBrands Brands')
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
