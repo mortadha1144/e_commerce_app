a1663938883f54186b16e89d7073101288201828
Mortadha Naser <mortadha114@gmail.com>
2023-07-17 21:20:54 +0300

    cart item card done & create check our cart

--- lib/Features/cart/presentation/views/cart_view.dart
+++ lib/Features/cart/presentation/views/cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/Features/cart/data/models/cart_model.dart';
 import 'package:e_commerce_app/Features/cart/presentation/views/widgets/cart_view_body.dart';
+import 'package:e_commerce_app/size_config.dart';
 import 'package:flutter/material.dart';
 
 class CartView extends StatelessWidget {
@@ -10,6 +11,7 @@ class CartView extends StatelessWidget {
     return Scaffold(
       appBar: buildAppBar(context),
       body: const CartViewBody(),
+      bottomNavigationBar: const CheckOurCart(),
     );
   }
 
@@ -26,6 +28,37 @@ class CartView extends StatelessWidget {
             style: Theme.of(context).textTheme.bodySmall,
           )
         ],
+      ),
+    );
+  }
+}
+
+class CheckOurCart extends StatelessWidget {
+  const CheckOurCart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(30),
+        vertical: getProportionateScreenWidth(10),
+      ),
+      height: 174,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+        boxShadow: [
+          BoxShadow(
+              offset: const Offset(0, -15),
+              blurRadius: 20,
+              color: const Color(0xFFDADADA).withOpacity(.15))
+        ],
+      ),
+      child: const Column(
+        children: [],
       ),
     );
   }
--- lib/Features/cart/presentation/views/widgets/cart_view_body.dart
+++ lib/Features/cart/presentation/views/widgets/cart_view_body.dart
@@ -1,45 +1,50 @@
 import 'package:e_commerce_app/Features/cart/data/models/cart_model.dart';
 import 'package:e_commerce_app/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class CartViewBody extends StatelessWidget {
+import 'cart_item_card.dart';
+
+class CartViewBody extends StatefulWidget {
   const CartViewBody({super.key});
 
+  @override
+  State<CartViewBody> createState() => _CartViewBodyState();
+}
+
+class _CartViewBodyState extends State<CartViewBody> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding:
           EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: 88,
-            child: AspectRatio(
-              aspectRatio: .88,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: const Color(0xFFF5F6F9),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Image.network(demoCarts[0].product.image!),
+      child: ListView.builder(
+        itemCount: demoCarts.length,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Dismissible(
+            key: Key(demoCarts[index].product.id.toString()),
+            direction: DismissDirection.endToStart,
+            background: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: const Color(0xFFFFE6E6),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                children: [
+                  const Spacer(),
+                  SvgPicture.asset('assets/icons/Trash.svg'),
+                ],
               ),
             ),
+            child: CartItemCard(card: demoCarts[index]),
+            onDismissed: (direction) {
+              setState(() {
+                demoCarts.removeAt(index);
+              });
+            },
           ),
-          const SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  demoCarts[0].product.title!,
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
-                  maxLines: 2,
-                )
-              ],
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
