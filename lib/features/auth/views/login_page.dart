d2dc49c7ff9a17f99e5305db846588e2c0cb3b83
Murtada Nasser <murtada.nasser@ta3leem.dev>
2025-05-17 20:04:11 +0300

    refactor: remove unused gap package import from login page

--- lib/features/auth/views/login_page.dart
+++ lib/features/auth/views/login_page.dart
@@ -20,7 +20,6 @@ import 'package:e_commerce_app/l10n/app_localizations.dart';
 import 'package:e_commerce_app/validator/validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_hook_mutation/riverpod_hook_mutation.dart';
